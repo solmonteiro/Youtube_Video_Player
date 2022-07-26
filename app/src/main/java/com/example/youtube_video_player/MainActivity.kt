816688c1e32c30ae8d4324b7e7438cd28fc21a51
solmonteiro <sole7mares@gmail.com>
2022-07-26 10:53:10 -0300

    API implementada

--- app/src/main/java/com/example/youtube_video_player/MainActivity.kt
+++ app/src/main/java/com/example/youtube_video_player/MainActivity.kt
@@ -1,11 +1,53 @@
 package com.example.youtube_video_player
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
+import com.example.youtube_video_player.R
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : YouTubeBaseActivity() {
+
+    // Change the AppCompactActivity to YouTubeBaseActivity()
+
+
+    val api_key = BuildConfig.API_KEY
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Get reference to the view of Video player
+        val ytPlayer = findViewById<YouTubePlayerView>(R.id.ytPlayer)
+
+        ytPlayer.initialize(api_key, object : YouTubePlayer.OnInitializedListener{
+            // Implement two methods by clicking on red error bulb
+            // inside onInitializationSuccess method
+            // add the video link or the
+            // playlist link that you want to play
+            // In here we also handle the play and pause functionality
+
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider?,
+                player: YouTubePlayer?,
+                p2: Boolean
+            ) {
+                player?.loadVideo("HzeK7g8cD0Y")
+                player?.play()
+            }
+
+            // Inside onInitializationFailure
+            // implement the failure functionality
+            // Here we will show toast
+            override fun onInitializationFailure(
+                p0: YouTubePlayer.Provider?,
+                p1: YouTubeInitializationResult?
+            ) {
+                Toast.makeText(this@MainActivity , "Video player Failed" , Toast.LENGTH_SHORT).show()
+            }
+        })
     }
 }
