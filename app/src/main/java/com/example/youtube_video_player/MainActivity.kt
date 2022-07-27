df5465f4fb089d708de51c3c15bb201c54dbc61d
solmonteiro <sole7mares@gmail.com>
2022-07-27 08:42:58 -0300

    Caixa de pesquisa implementada

--- app/src/main/java/com/example/youtube_video_player/MainActivity.kt
+++ app/src/main/java/com/example/youtube_video_player/MainActivity.kt
@@ -11,24 +11,16 @@ import com.example.youtube_video_player.R
 
 class MainActivity : YouTubeBaseActivity() {
 
-    // Change the AppCompactActivity to YouTubeBaseActivity()
-
-
     val api_key = BuildConfig.API_KEY
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Get reference to the view of Video player
         val ytPlayer = findViewById<YouTubePlayerView>(R.id.ytPlayer)
 
         ytPlayer.initialize(api_key, object : YouTubePlayer.OnInitializedListener{
-            // Implement two methods by clicking on red error bulb
-            // inside onInitializationSuccess method
-            // add the video link or the
-            // playlist link that you want to play
-            // In here we also handle the play and pause functionality
+
 
             override fun onInitializationSuccess(
                 provider: YouTubePlayer.Provider?,
@@ -39,9 +31,6 @@ class MainActivity : YouTubeBaseActivity() {
                 player?.play()
             }
 
-            // Inside onInitializationFailure
-            // implement the failure functionality
-            // Here we will show toast
             override fun onInitializationFailure(
                 p0: YouTubePlayer.Provider?,
                 p1: YouTubeInitializationResult?
